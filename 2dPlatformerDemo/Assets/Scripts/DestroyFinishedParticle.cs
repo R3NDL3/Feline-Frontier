f5f49378da96a69c14b8fc58b45d1de6bdd04d30
TheDerpySage <majoradecamp@gmail.com>
2015-04-08 17:26:17 -0500

    Small but big fix
    Fixed Particles not being destroyed after use, clogging system memory
    the more times you die.

---
+++ 2dPlatformerDemo/Assets/Scripts/DestroyFinishedParticle.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyFinishedParticle : MonoBehaviour {
+	private ParticleSystem thisParticleSystem;
+	public void Start(){
+		thisParticleSystem = GetComponent<ParticleSystem> ();
+	}
+	public void Update () {
+	if (thisParticleSystem.isStopped)
+			Destroy (gameObject);
+	}
+}
